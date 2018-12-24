6c45d9b9bc774d8ddd447a36c6ee9d6d21a146e4
gopal <gopal@stackmint.com>
2018-12-24 19:23:12 +0530

    Quiz App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,41 +10,73 @@ export class AppComponent implements OnInit {
   quizes: any[] = [];
   index: number;
   question: any;
-  answers: any;
+  answers: any[];
+  slide: any;
   counter: number = 0;
   priv: number;
   next: number;
+  saveanswer: any[] = [];
+  Value: any;
+  option: any;
+  selectedopt: any;
 
+  
   constructor(private quizService: QuizService) { }
 
-  ngOnInit():void {
+  ngOnInit(): void {
     // this.index= 0;
     // var i = this.index;
     this.quizService.getQuestions().subscribe(res => {
       this.quizes.push(res.data);
-
+      this.answers = res.data;
+      this.slide = res.data;
+    
+      // console.log(this.answers);
       console.log(this.quizes);
     }, error => {
       console.log(error);
     })
   }
-  nextQuestion():void {
+
+  nextQuestion(value: any): void {
+    console.log(value)
     this.priv = this.counter;
     // console.log(this.question);
     if (this.quizes[0].length > this.counter) {
       // const element = this.quizes[0].this.counter
       // this.question = element[this.counter].Question;
       this.counter = this.counter + 1;
+      this.saveanswer.push(this.option);
+      // this.saveanswer.push(this.slide);
+      console.log(this.saveanswer);
     } else {
       console.log(this.quizes);
     }
   }
-  previousQuestion():void {
+
+  previousQuestion(): void {
 
     if (this.quizes[0].data > this.priv) {
-      // console.log(this.question);
+      
+      // console.log(this.question); 
+      
       const element = this.quizes[0].this.counter
-      this.question = element[this.counter].Question;
+        this.question = element[this.counter].Question;
+        this.selectedopt = this.saveanswer[this.priv].option;
+        if(element.Control.MetaData) {
+          this.next = this.counter;
+          // this.priv = this.next;
+          this.selectedopt = this.saveanswer[this.priv];
+          console.log(this.selectedopt);
+        }
+      // if(this.quizes[0].data > this.priv) {
+      //   const element = this.quizes[0].this.counter
+      //   this.question = element[this.counter].Question;
+      //   this.selectedopt = this.saveanswer[this.priv].option;
+      // }
+      
+      
+      // console.log(this.selectedopt);
 
     } else {
       this.counter = this.counter - 1;
