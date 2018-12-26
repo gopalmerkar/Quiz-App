5f14effbf46224efd815ced5b711615c1358e200
gopal <gopal@stackmint.com>
2018-12-26 20:14:58 +0530

    Quiz App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,10 @@ export class AppComponent implements OnInit {
   saveanswer: any[] = [];
   Value: any;
   option: any;
-  selectedopt: any;
-
+  tempArray: {};
+  selectdOption: any;
   
+ 
   constructor(private quizService: QuizService) { }
 
   ngOnInit(): void {
@@ -30,7 +31,7 @@ export class AppComponent implements OnInit {
       this.quizes.push(res.data);
       this.answers = res.data;
       this.slide = res.data;
-    
+
       // console.log(this.answers);
       console.log(this.quizes);
     }, error => {
@@ -46,42 +47,92 @@ export class AppComponent implements OnInit {
       // const element = this.quizes[0].this.counter
       // this.question = element[this.counter].Question;
       this.counter = this.counter + 1;
-      this.saveanswer.push(this.option);
+      // this.saveanswer.push(this.option);
+      this.saveanswer.push(this.tempArray)
+      // console.log(this.tempArray);
       // this.saveanswer.push(this.slide);
       console.log(this.saveanswer);
     } else {
       console.log(this.quizes);
     }
+   
+    
   }
 
-  previousQuestion(): void {
+
+  onChange(value,option,optionId) {
+    console.log("value:",value);
+    console.log("optionId:",optionId);
+    console.log("option:",option);
+
+
+    this.tempArray = {
+      selectdOption: value,
+      optionId: optionId,
+      option:option
+    }
+    
+    
+  }
+
+  previousQuestion(index): void {
+    this.saveanswer.slice(index, 1);
+    console.log(this.saveanswer);
+    console.log(index);
 
     if (this.quizes[0].data > this.priv) {
-      
-      // console.log(this.question); 
-      
+
       const element = this.quizes[0].this.counter
-        this.question = element[this.counter].Question;
-        this.selectedopt = this.saveanswer[this.priv].option;
-        if(element.Control.MetaData) {
-          this.next = this.counter;
-          // this.priv = this.next;
-          this.selectedopt = this.saveanswer[this.priv];
-          console.log(this.selectedopt);
-        }
+      this.question = element[this.counter].Question; 
+      this.counter = this.priv;
+       //this.answers = element.Control.MetaData;
+      // this.selectdOption = this.saveanswer[this.priv];
+      this.answers = this.saveanswer[this.priv].option;
+      // this.option = this.answers;
+      console.log(this.answers);
+      
+        
+        //this.answers = this.selectdOption.Value;
+        // console.log(this.answers);
+      
+    
+        
+     
+    //    if (element.Control.MetaData) {
+    //   this.next = this.counter;
+    //    this.priv = this.next;
+    //   this.selectedopt = this.saveanswer[this.priv];
+    //   console.log(this.selectedopt);
+    //  }
       // if(this.quizes[0].data > this.priv) {
       //   const element = this.quizes[0].this.counter
       //   this.question = element[this.counter].Question;
       //   this.selectedopt = this.saveanswer[this.priv].option;
       // }
-      
-      
+
+
       // console.log(this.selectedopt);
 
     } else {
       this.counter = this.counter - 1;
+      this.answers = this.saveanswer[this.priv];
+      
     }
-
+    this.saveanswer[index];
+    console.log(this.saveanswer[index]);
   }
+
+  // previousQuestion(index): void {
+    
+  //    if(this.quizes[0].data > this.priv) {
+  //      if(this.quizes[0].data[this.priv]) {
+  //       const element = this.quizes[0].this.counter
+  //       this.question = element[this.counter].Question;
+  //       this.answers = element.Control.MetaData;
+  //       this.selectdOption = this.saveanswer[this.priv].option;
+  //      }
+  //    }
+  // }
+
 }
 
